feeb891045bcf6288f8069e7825fa890d539d7af
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-23 10:13:31 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -2,24 +2,21 @@
 
 using namespace std;
 
-int check(int n){
+int check(long n){
 int c=0;
 for(int i=2;i<n;i++){
-    if(n%i!=0){ c++;}
+    if(n%i==0){return 0; }
 }
-
-if(c==1){return 1;}
-else{return 0;}
+return 1;
 }
 
 int main(){
-    int c=0,d=0,n=2;
+    int c=0,n=2;
 while (1){
-        d=check(n);
-    if(d==1){c++;}
+    if(check(n)){c++;}
     if(c==10001){cout<<n; break;}
     n++;
-    d=0;
+
 }
 
 
