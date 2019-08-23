1163f379c4734fa70397b4ba248fa42239e42a7b
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-23 12:37:29 +0530

    Update q10.cpp

--- q10.cpp
+++ q10.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(){
-unsigned long i,f=1,j,s=0;
-while(i>=2000000){
+unsigned long i=3,f=1,j,s=0;
+while(i<=2000000){
     for(j=3;j<i;j+=2){
           if(i%j==0){f=0;}
                         }
 
     if(f==1){ s=s+i+2;      }
 
-    i++;
+    i+=2;
 
 
     f=1;
