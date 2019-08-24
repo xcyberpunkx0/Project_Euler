fef1cbfe06273052f17e232c05d9cafb1ee518dd
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-24 21:37:00 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
 int check(long n){
-for(int i=2;i<n;i++){
-    if(n%i==0){return 0; }
-}
-return 1;
+        
+    if(n <= 0 || n == 1 || (n % 2 == 0 && n != 2)){  return 0;         }
+            
+        for(int i=3;i<=sqrt(n);i+=2){
+                    
+                if(n%i==0){return 0; }
+                                        }
+        return 1;
 }
 
 int main(){
-    int c=0,n=2;
-while (1){
-    if(check(n)){c++;}
-    if(c==10001){cout<<n; break;}
-    n++;
+    int c=0,n=1;
+            while (1){
+                if(check(n)){c++;}
+                if(c==10001){cout<<n; break;}
+                n++;
 
-}
+                        }
 
 
 return 0;
 
 
 }
-
