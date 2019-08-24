c57e27ad2b5c9d57fed4424e578e062975f205c8
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-24 22:23:01 +0530

    Update q10.cpp

--- q10.cpp
+++ q10.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
-
+#include<math.h>
 using namespace std;
 
 int main(){
 unsigned long i=3,f=1,j,s=0;
-while(i<=2000000){
-    for(j=3;j<i;j+=2){
-          if(i%j==0){f=0;}
-                        }
-
-    if(f==1){ s=s+i+2;      }
-
-    i+=2;
+while(i<=200000){
 
 
-    f=1;
+        for(j=3;j<sqrt(i);j+=2){
+                if(i%j==0){f=0;}
+                                    }
+
+        if(f==1){ s=s+i+2;      }
+
+            i+=2;
+
+
+            f=1;
 
 }
 
