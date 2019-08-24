307250d7b5a4a54c377efede6493a7add7bf4841
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-25 00:58:35 +0530

    Update Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,22 +1,24 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
-
-
 int main()
-{   int lcm=20;
-      while(1){
-        if( lcm%18==0 && lcm%11==0 && lcm%12==0&& lcm%13==0 && lcm%14==0 && lcm%15==0 && lcm%16==0 && lcm%17==0 && lcm%19==0 && lcm%20==0){
-            cout<<lcm;
-            break;
+{
+    int lcm=0,a=1,b=20;
+
+    for(int i=1;i<=20;i++){
+            int b=i;
+
+                    if(a>b) {   lcm=a;            }
+                        else{    lcm=b;         }
+
+        while(1){
+            if(lcm%a==0&&lcm%b==0){
+
+                a=lcm;
+                break;
+            }
+            lcm++;
+
         }
-        lcm++;
-
-
-
     }
-    return 0;
-}
 
-
-
+cout<<lcm;
