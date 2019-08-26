6015b07f1f9313c8f65117a868ededcccc6dfbfb
xcyberpunkx0 <53346087+xcyberpunkx0@users.noreply.github.com>
2019-08-26 08:59:25 +0530

    Update q7.cpp

--- q7.cpp
+++ q7.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int check(long n){
-        
+
     if(n <= 0 || n == 1 || (n % 2 == 0 && n != 2)){  return 0;         }
-            
+
         for(int i=3;i<=sqrt(n);i+=2){
-                    
+
                 if(n%i==0){return 0; }
                                         }
         return 1;
@@ -16,13 +16,13 @@ int check(long n){
 
 int main(){
     int c=0,n=1;
-            while (1){
+            while (c!=10001){
                 if(check(n)){c++;}
-                if(c==10001){cout<<n; break;}
-                n++;
+
+                ++n;
 
                         }
-
+                cout<<n-1;
 
 return 0;
 
